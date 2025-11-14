57e136d35a0ba45b890b2428eff14d42171ed9df
Rosieee <baring.jp@obmontessori.edu.ph>
2025-11-14 09:01:26 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+# Student Clubs
+
+from pyscript import display  # type: ignore
+
+Art_Club = {'ꜱʜᴀʟᴀɴɪᴇ ɢᴀʀᴀʙɪʟᴇꜱ', 'ᴀᴍᴀɴᴅᴀ ʏᴀᴏ', 'ɴɪʟᴢᴀ ᴄᴀʟᴀɴɢʟᴀɴɢ'}
+COCC = {'ᴀɪꜱʜᴀ ʟᴇᴅᴇꜱᴍᴀ', 'ᴋᴏʙʏ ʙᴀʏʟᴏɴ', 'ᴍᴀʀʏᴀᴍ ʟᴇᴅᴇꜱᴍᴀ'}
+
+display('ᴀʀᴛ ᴄʟᴜʙ ᴍᴇᴍʙᴇʀꜱ:', target='output')
+display(Art_Club, target='output')
+display('ᴄᴏᴄᴄ ᴍᴇᴍʙᴇʀꜱ:', target='output')
+display(COCC, target='output')
+
+display('(ᴀᴛ ʟᴇᴀꜱᴛ ᴏɴᴇ ᴄʟᴜʙ):', target='output')
+display(Art_Club | COCC, target="output")
+
+display('(Both clubs):', target='output')
+display(Art_Club & COCC, target='output')
+
+display('ᴏɴʟʏ ɪɴ ᴀʀᴛ ᴄʟᴜʙ:', target='output')
+display(Art_Club - COCC, target='output')
+
+display('ᴏɴʟʏ ɪɴ ᴄᴏᴄᴄ:', target='output')
+display(COCC - Art_Club, target='output')
+
+display('ᴇxᴀᴄᴛʟʏ ᴏɴᴇ ᴄʟᴜʙ:', target='output')
+display(Art_Club ^ COCC, target='output')
